d546d8d426890878d0d36b173a881d07f96ddc6c
Jesse <Jessepan123456@gmail.com>
2026-05-23 17:32:43 -0600

    first commit

---
+++ src/main.rs
@@ -0,0 +1,12 @@
+fn main() {
+    println!("Hello, world!");
+}
+
+// --- Plan ---
+//1. Send URL link
+//2. Generate a ID that unique to that URL
+//3. Store it with that ID
+//4. Return the shortener link
+
+//Add a clickable for that new link
+//Maps to that correct URL Link
