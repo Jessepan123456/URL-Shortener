0665ebb0fef3175b6f343e3a103beb47a09a0b2f
Jesse <Jessepan123456@gmail.com>
2026-05-25 22:03:40 -0600

    Tested out how to GET and POST

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,57 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{
+    Json, Router,
+    routing::{get, post},
+    serve,
+};
+use serde::Deserialize;
+use std::collections::HashMap;
+use tokio::net::TcpListener;
+
+
+#[derive(Deserialize)]
+struct Request {
+    url: String,
 }
+
+#[tokio::main]
+async fn main() {
+    //Makes the website server
+    let app = Router::new()
+        .route("/", get(rust_page));
+
+    //Listen for what server it makes
+    let listener = TcpListener::bind("127.0.0.1:3000").await.unwrap();
+
+    println!("Server running on http://127.0.0.1:3000");
+
+    serve(listener, app).await.unwrap();
+}
+
+async fn rust_page() -> String {
+    let mut url_id: HashMap<String, String> = HashMap::new();
+    let id = nanoid::nanoid!(6);
+
+    url_id.insert(id.clone(), "https//youtube.com".to_string());
+
+    match url_id.get(&id) {
+        Some(url) => url.clone(),
+        None => "No URL found".to_string(),
+    }
+}
+
+async fn shorten(Json(body): Json<Request>) -> String {
+    format!("Got URL: {}", body.url)
+}
+
+// fn main() {
+//     let mut url_id: HashMap<String, String> = HashMap::new();
+//     let id = nanoid::nanoid!(6);
+//     println!("Nano ID: {}", id);
+
+//     url_id.insert(id.to_string(), "https//youtube.com".to_string());
+
+//     println!("{:?}", url_id)
+// }
 
 // --- Plan ---
 //1. Send URL link
